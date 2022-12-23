2adfe4aa0a0568da2944f2ddbd5fd83398baeda6
andrebenedetti <andredbenedetti@gmail.com>
2022-12-23 20:15:44 -0300

    layout improvements for bigger screens

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,19 +1,21 @@
-
-import clsx from 'clsx'
-import React from 'react'
+import clsx from "clsx";
+import React from "react";
 
 interface Props {
-  label: string
-  className?: string
+  label: string;
+  className?: string;
 }
 
-function Button({
-  label,
-  className
-}: Props) {
+function Button({ label, className }: Props) {
   return (
-    <button className={clsx(`text-gray-900 py-3 mx-2 hover:opacity-75 hover:border hover:border-2 box-content border-gray-900  ${className}`)}>{label}</button>
-  )
+    <button
+      className={clsx(
+        `text-gray-900 py-3 mx-2 hover:opacity-75 border-gray-900  ${className}`
+      )}
+    >
+      {label}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Button from '../components/Button'
-import GradePanel from '../components/GradePanel'
+import Head from "next/head";
+import Button from "../components/Button";
+import GradePanel from "../components/GradePanel";
 
 export default function Home() {
   return (
@@ -11,10 +11,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main >
-        <h1 className='text-center py-4'>Hello, Andre =)<br /><strong>how was your day, today?</strong></h1>
-        <GradePanel />
+      <main>
+        <h1 className="text-center py-4">
+          Hello, Andre =)
+          <br />
+          <strong>how was your day, today?</strong>
+        </h1>
+        <div className="flex justify-items-center ml-auto mr-auto max-w-xl">
+          <GradePanel />
+        </div>
       </main>
     </>
-  )
+  );
 }
